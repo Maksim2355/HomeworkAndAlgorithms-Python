f3ab302161155a8fb2237462aca275cc7c3b1360
max <provotorov_maksim@mail.ru>
2019-04-08 19:14:11 +0300

    Initial commit

--- Matcrica.py
+++ Matcrica.py
@@ -1,5 +1,5 @@
 from random import randint
-matrix_size = int(input("Введите размер матрицы"))
+matrix_size = int(input("Введите размер матрицы:"))
 
 
 def column_formation(): #Формирование столбца
@@ -18,22 +18,21 @@ print(matrix)
 
 
 #Поиск минора элемента
-def minor_extra(strokaudalenia, matrix_size, matrix):
-    minor = matrix
+def minor_extra(strokaudalenia, matrix_size, minor):
     del minor[strokaudalenia]
-    for k in range(matrix_size):
+    for k in range(matrix_size - 2):
         del minor[k][0]
     return det(minor, matrix_size - 1)
 
 
-
-
+#Нахождение определителя
 def det(matrix, matrix_size):
+
     if len(matrix) == 1:
         return matrix[0][0]
     elif len(matrix) == 2:
-         det2x2 = (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1])
-         return det2x2
+        det2x2 = (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1])
+        return det2x2
     else:
         detMatrix = 0
         for j in range(matrix_size):
